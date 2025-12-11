cf482f99f17af9f647d08ab1b1fb265d74dbc31c
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-12-11 16:33:42 +0530

    Adding project files

---
+++ snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+SEGMENT_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in SEGMENT_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for i in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[i - 1].xcor()
+            new_y = self.segments[i - 1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
