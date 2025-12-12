cfce58aaf16752823d4859107895aaad28920196
Sushant Shirsekar <63793660+sushantshirsekar@users.noreply.github.com>
2025-12-12 22:17:17 +0530

    Adding score and wall collision

--- scoreboard.py
+++ scoreboard.py
@@ -1,1 +1,29 @@
-# placeholder
+from turtle import Turtle
+
+# Constants
+FONT = ("courier", 20, "bold")
+ALIGNMENT = "center"
+MOVE = False
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.update_score()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER!", move=MOVE, align=ALIGNMENT, font=FONT)
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score = {self.score}", move=MOVE, align=ALIGNMENT, font=FONT)
+
